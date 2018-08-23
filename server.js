5c232e83b565a140bd4d11d39987ba86e7dcf38c
William Seto <wseto2@myseneca.ca>
2018-08-23 18:31:45 -0400

    bug

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ io.on('connection', function (socket) {
             io.emit('remove user', userName);
             for (let i = 0; i < gameList.length; i++) {
                 if (gameList[i].hostPlayer == socket.id || gameList[i].player == socket.id) {
-                    gameList.splice(i, 1);
                     io.to(gameList[i].hostPlayer).emit('leave game');
                     io.emit('remove game', gameList[i]);
+                    gameList.splice(i, 1);
                     break;
                 }
             }
