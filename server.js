a12a28795a6ca1959ec063fc8ee17baf479af60e
William Seto <wseto2@myseneca.ca>
2018-08-23 16:11:40 -0400

    changes

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ io.on('connection', function (socket) {
     socket.on('change state', function (column, game, color) {
         for (let i = 0; i < gameList.length; i++) {
             if (gameList[i].hostPlayer == game.hostPlayer) {
-                gameList[i].grid.columns[column].cells[gameList[i].grid.columns[column].count++].color = color;
+                gameList[i].grid.columns[column].cells[gameList[i].grid.columns[column].count].color = color;
+                gameList[i].grid.columns[column].count++;
                 io.to(game.hostPlayer).emit('change state', (column, gameList[i].grid.columns[column].count));
             }
         }
